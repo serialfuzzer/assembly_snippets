4516f7fac717af7f75b54ba1707c7e43d9ba2b34
Suyogya <suyogkhanal4@gmail.com>
2023-12-14 09:40:02 +0530

    added comments

--- findMax/max.s
+++ findMax/max.s
@@ -18,14 +18,18 @@ data_items: #These are the data items
 .globl _start
 _start:
 movl $0, %edi # move 0 into the index register
-movl data_items(,%edi,4), %eax # load the first byte of data
+movl data_items(,%edi,4), %eax # load the first byte of data, this is indexed addressing mode
 movl %eax, %ebx # since this is the first item, %eax is
 # the biggest
 start_loop: # start loop
 cmpl $0, %eax # check to see if we’ve hit the end
+# cmpl is used to compare the first operand with the second one
 je loop_exit
+# je = jump if equals, jump to loop_exit section if eax contains 0  
 incl %edi # load next value
+# incl = increment the value in %edi by one
 movl data_items(,%edi,4), %eax
+# in other words move the value at data_items[1] to eax
 cmpl %ebx, %eax # compare values
 jle start_loop # jump to loop beginning if the new
 # one isn’t bigger
