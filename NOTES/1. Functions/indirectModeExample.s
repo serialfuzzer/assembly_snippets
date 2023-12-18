1861cb47bb5bbee01ecebf26275444872578f7be
Suyogya <suyogkhanal4@gmail.com>
2023-12-19 00:39:20 +0530

    Updated comments

--- NOTES/1. Functions/indirectModeExample.s
+++ NOTES/1. Functions/indirectModeExample.s
@@ -11,6 +11,5 @@ _start:
 
     # Your code continues here...
 
-    # For example, you can print the value in EBX (4 in this case)
     movl $1, %eax            # System call number for write
     int $0x80                # Trigger the kernel interrupt
