5daff6c86c8fd6aaf4b27dd5e478b041e2a80018
Suyogya <suyogkhanal4@gmail.com>
2023-12-20 00:03:09 +0530

    Fixed bug

--- NOTES/1. Functions/stack_demo.s
+++ NOTES/1. Functions/stack_demo.s
@@ -4,5 +4,5 @@ _start:
 pushl $4 # push 4 to top of the stack
 pushl $3 # push 3 to top of the stack
 movl $1, %eax # code for exit system call
-movl %esp, %ebx # put whats at top of the stack in the ebx register, which contains the exit status code
+movl (%esp), %ebx # put whats at top of the stack in the ebx register, which contains the exit status code
 int $0x80 # interrupt for syscall
